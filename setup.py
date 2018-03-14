85f0050a59726e96ce0ac6f995183d14958091fa
gbenatt1 <gabriel.b.alvim@gmail.com>
2018-03-13 22:10:49 -0300

    Adding setup.py for the project

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+
+DEPENDENCIES = [
+    "Cython==0.27.2",
+    "scipy",
+    "dask",
+    "pyarrow",
+    "pandas",
+    "implicit==0.2.7"
+]
+
+setup(
+    name='ml-challenge',
+    version=VERSION,
+    description='Machine Learning Challenge',
+    author='Gabriel Alvim',
+    author_email='gabriel.b.alvim@gmail.com',
+    packages=find_packages(),
+    install_requires=DEPENDENCIES,  # external packages as dependencies
+)
